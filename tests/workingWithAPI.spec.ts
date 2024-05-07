e2e651169a62aff4f032e40e69becfae7a450609
Jéssica Luz <qae.jessicaluz@gmail.com>
2024-05-07 11:33:22 +0100

    Mocking APIs concept. Defining route and providing our on response

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -1,9 +1,23 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
+import tags from "../test-data/tags.json";
 
-test.beforeEach(async ({page}) => {
-    await page.goto('https://conduit.bondaracademy.com/')
-})
+test.beforeEach(async ({ page }) => {
+  /* When we want to create a mock, we need to configure it inside 
+    of the playwright framework before browser make a call to a certain API.
+    Otherwise playwright will not know which API should be intercepted.*/
 
-test('first test', async ({page}) => {
-    await expect(page.locator('.navbar-brand')).toHaveText('conduit');
-})
+  await page.route(
+    "*/**/api/tags",
+    async (route) => {
+      await route.fulfill({
+        body: JSON.stringify(tags),
+      });
+    }
+  );
+  await page.goto("https://conduit.bondaracademy.com/");
+  await page.waitForTimeout(500);
+});
+
+test("first test", async ({ page }) => {
+  await expect(page.locator(".navbar-brand")).toHaveText("conduit");
+});
